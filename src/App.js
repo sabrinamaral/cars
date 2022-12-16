5dd134a69a536d37fa7b136807025a54d38b4faa
sabrinamaral <sabrinamaral@hotmail.com>
2022-12-15 22:05:50 -0300

    adding style

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import CarValue from "./components/CarValue";
 
 const App = () => {
   return (
-    <div>
+    <div className="container is-fluid">
       <CarForm />
-      <CarList />
       <CarSearch />
+      <CarList />
       <CarValue />
     </div>
   );
