423acc7bcfae11f1a0b24c63b4517246ebe80679
sabrinamaral <sabrinamaral@hotmail.com>
2022-12-15 22:07:19 -0300

    rendering the list of cars | CarList

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,7 +1,38 @@
-import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeCar } from "../store";
 
 const CarList = () => {
-  return <div>CarList</div>;
+  const dispatch = useDispatch();
+
+  const cars = useSelector((state) => {
+    return state.cars.data;
+  });
+
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id));
+  };
+  const renderedCar = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {renderedCar}
+      <hr />
+    </div>
+  );
 };
 
 export default CarList;
