da28df51194310803914a5f2a7d8b045eda9f7c2
sabrinamaral <sabrinamaral@hotmail.com>
2022-12-16 22:58:17 -0300

    carValue

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,7 +1,20 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
 const CarValue = () => {
-  return <div>CarValue</div>;
-};
+  const totalCost = useSelector(({ cars: { searchTerm, data } }) =>
+    data
+      .filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .reduce((acc, car) => acc + car.cost, 0)
+  );
 
+  //   let cost = 0;
+  //   for (let car of filteredCars) {
+  //     cost += car.cost;
+  //   }
+  //   return cost;
+
+  return <div className="car-value">Total Cost:${totalCost}</div>;
+};
 export default CarValue;
